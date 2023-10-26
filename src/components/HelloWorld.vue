38f86fc85e09d67db341c6d2320beaf225953f21
liminliu <liminliu@microsoft.com>
2023-10-26 13:55:01 +0800

    fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,7 @@
 <template>
+  <!-- eslint-disable  -->
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
@@ -24,20 +14,71 @@
     <hr/>
     <h3>Reactive Data Example</h3>
     <ReactiveExample />
+    <hr/>
+    <h2>render list with v-for directive</h2>
+    <ul>
+      <li v-for=" (item, idx) in list" :key="idx">
+        <a :href="item.url" target="_blank" rel="noopener">{{item.text}}</a>
+      </li>
+    </ul>
+    <input @change="changeInput" /> 
+
+    <hr/>
+    <h2>V-model example</h2>
+    <label>value bind to v-model: {{ modelName }}</label>
+    <VModelExample
+      v-model="modelName"
+    ></VModelExample>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 import ReactiveExample from './ReactiveExample.vue';
+import * as Vue from 'vue';
+import VModel from './VModel.vue';
 
 export default {
   name: 'HelloWorld',
   components: {
-    ReactiveExample: ReactiveExample
+    ReactiveExample: ReactiveExample,
+    VModelExample: VModel 
   },
   props: {
     msg: String
-  }
+  },
+  watch: {
+    // list: function (newVal, oldVal) {
+      
+    // }
+  },
+  data: () => {
+    return {
+      list: [{
+        url: "https://vuejs.org",
+        text: 'Core Docs'
+      }, {
+        url: "https://router.vuejs.org",
+        text: 'Forum'
+      }, {
+        url: "https://chat.vuejs.org",
+        text: 'Community Chat'
+      }, {
+        url: "https://twitter.com/vuejs",
+        text: 'Twitter'
+      },{
+        url: "https://news.vuejs.org",
+        text: 'News'
+      }],
+      modelName: 'v-model example',
+    }
+  },
+  methods: {
+    changeInput: function (e) {
+      Vue.set(this.list, 0, {...this.list[0], ...{text: e.target.value}});
+    }
+  },
+
 }
 </script>
 
---
+++ src/components/VModel.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: "VModelExample",
+  model: {
+    event: 'update:placeholderName',
+    prop: 'placeholderName'
+  },
+  props: {
+    placeholderName: String,
+  },
+  emits: ['update:placeholderName'],
+  mounted() {
+    console.log(this.value);
+  }
+}
+</script>
+
+<template>
+  <div>
+    <label>Type to change: </label>
+    <input
+    type="text"
+    :placeholder="placeholderName"
+    :value="placeholderName"
+    @input="$emit('update:placeholderName', $event.target.value)"
+  />
+
+  </div>
+</template>
